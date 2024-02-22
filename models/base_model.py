2b8ca5caf72abfd1df34add336e827f55b5a7a9c
MissPhumy <missphumy@gmail.com>
2024-02-22 03:34:32 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,98 +1,83 @@
 #!/usr/bin/python3
-"""
-Module base_model
-has class BaseModel that defines all
-common atributes/methods for other classes
-"""
-import models
-import sqlalchemy
+"""This is the base model class for AirBnB"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, DateTime
 import uuid
-from os import getenv
+import models
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    Base = declarative_base()
-else:
-    Base = object
+Base = declarative_base()
 
 
 class BaseModel:
+    """This class will defines all common attributes/methods
+    for other classes
     """
-    public instance attributes:
-    id, created_at, updated_at
-    """
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, nullable=False,
-                            default=datetime.utcnow())
-        updated_at = Column(DateTime, nullable=False,
-                            default=datetime.utcnow())
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
 
     def __init__(self, *args, **kwargs):
-        """
-        Initialization
-        id uses uuid.uuid4(). Regenerates a unique id for each BaseModel.
-        updated_at -> datetime will be updated everytime an object is changed.
-        *args and **kwargs are used as constructors
+        """Instantiation of base model class
+        Args:
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
         """
         if kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    v = datetime.strptime(v, time)
-                if k != "__class__":
-                    setattr(self, k, v)
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            """ models.storage.new(self)"""
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
+        """returns a string
+        Return:
+            returns a string of class name, id, and dictionary
         """
-        Returns string rep of class BaseModel
-        Prints: "[<class name>] (<self.id>) <self.__dict__>"
-        """
-        return ("[{}] ({}) {}".
-                format(self.__class__.__name__, self.id, self.__dict__))
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
 
     def __repr__(self):
+        """return a string representaion
         """
-        Returns the string representation of BaseModel
-        calls __str__()
-        """
-        return (self.__str__())
+        return self.__str__()
 
     def save(self):
+        """updates the public instance attribute updated_at to current
         """
-        updates the public instance attribute updated_at
-        with the current datetime
-        Calls save(self) method of storage
-        """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
-        new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-        return new_dict
+        """creates dictionary of the class  and returns
+        Return:
+            returns a dictionary of all the key values in __dict__
+        """
+        my_dict = dict(self.__dict__)
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in my_dict.keys():
+            del my_dict['_sa_instance_state']
+        return my_dict
 
     def delete(self):
-        """
-        deletes instance from storage
+        """ delete object
         """
         models.storage.delete(self)
--- models/review.py
+++ models/review.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-import models
+"""This is the review class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "reviews"
-        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        text = Column(String(1024), nullable=False)
-
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
-
-    def __init__(self, *args, **kwargs):
-        """intialize review"""
-        super().__init__(*args, **kwargs)
+    """This is the class for Review
+    Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
